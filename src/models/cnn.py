ab6f567e7056db082b07a63690cfd6cd7333b6c3
nhatsmrt <nhatsmrt@uw.edu>
2019-11-17 13:07:47 -0800

    Add batch norm to CNN model.

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -8,9 +8,11 @@ class CNNModel(nn.Sequential):
     def __init__(self):
         super().__init__(
             nn.Conv1d(40, 64, 3),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features=64),
             nn.Conv1d(64, 128, 3),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features=128),
             AveragePool(dim=2),
             nn.Linear(128, 6)
         )
