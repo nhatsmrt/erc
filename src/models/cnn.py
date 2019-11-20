04310bed096caafbf17bd5f159af144858c8c530
nhatsmrt <nhatsmrt@uw.edu>
2019-11-20 09:42:34 -0800

    Set up test for deeper cnn.

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,7 +1,7 @@
 from torch import nn
 from nntoolbox.vision.components import *
 
-__all__ = ['CNNModel', 'CNNModelV2', 'DeepCNNModel']
+__all__ = ['CNNModel', 'MediumCNNModel', 'DeepCNNModel']
 
 
 class CNNModel(nn.Sequential):
@@ -19,31 +19,33 @@ class CNNModel(nn.Sequential):
         )
 
 
-class CNNModelV2(nn.Sequential):
+class MediumCNNModel(nn.Sequential):
     def __init__(self):
         super().__init__(
-            ConvolutionalLayer(1, 16, 5),
+            ConvolutionalLayer(1, 8, 5),
+            ResidualBlockPreActivation(16),
+            ConvolutionalLayer(8, 16, 3, stride=2),
             ResidualBlockPreActivation(16),
             nn.AdaptiveAvgPool2d(4),
             Flatten(),
             nn.Dropout(p=0.5),
-            nn.Linear(256, 6),
+            nn.Linear(16 * 4 * 4, 6),
         )
 
 
 class DeepCNNModel(nn.Sequential):
     def __init__(self):
         super().__init__(
-            ConvolutionalLayer(1, 16, 5),
+            ConvolutionalLayer(1, 8, 5),
+            ResidualBlockPreActivation(8),
+            ConvolutionalLayer(8, 16, 3, stride=2),
             ResidualBlockPreActivation(16),
-            ConvolutionalLayer(16, 64, 3, stride=2),
-            ResidualBlockPreActivation(64),
-            ConvolutionalLayer(64, 128, 3, stride=2),
-            ResidualBlockPreActivation(128),
-            nn.AdaptiveAvgPool2d((2, 4)),
+            ConvolutionalLayer(16, 32, 3, stride=2),
+            ResidualBlockPreActivation(32),
+            nn.AdaptiveAvgPool2d(4),
             Flatten(),
             nn.Dropout(p=0.5),
-            nn.Linear(2 * 4 * 128, 128),
+            nn.Linear(4 * 4 * 32, 128),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(128, 6),
