a637e7be525b8b17e39f9151d2ee18f468d4753d
nhatsmrt <nhatsmrt@uw.edu>
2019-11-20 16:50:51 -0800

    Set up test for ResNet18.

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,7 +1,8 @@
 from torch import nn
 from nntoolbox.vision.components import *
+from torchvision.models import resnet18
 
-__all__ = ['CNNModel', 'MediumCNNModel', 'DeepCNNModel']
+__all__ = ['CNNModel', 'MediumCNNModel', 'DeepCNNModel', 'ResNet18']
 
 
 class CNNModel(nn.Sequential):
@@ -40,3 +41,17 @@ class DeepCNNModel(nn.Sequential):
             ResidualBlockPreActivation(32),
             FeedforwardBlock(in_channels=32, out_features=6, pool_output_size=4, hidden_layer_sizes=(128,), drop_p=0.5)
         )
+
+
+class ResNet18(nn.Sequential):
+    def __init__(self):
+        base = resnet18()
+        layers = [
+            base.conv1, base.bn1, base.relu, base.maxpool,
+            base.layer1, base.layer2, base.layer3, base.layer4,
+            FeedforwardBlock(
+                in_channels=512, out_features=6, pool_output_size=1,
+                hidden_layer_sizes=(128,), drop_p=0.5
+            )
+        ]
+        super().__init__(*layers)
--- train.py
+++ train.py
@@ -57,7 +57,8 @@ train_data, val_data = random_split_before_transform(
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_data, batch_size=batch_size)
 
-model = DeepCNNModel()
+# model = DeepCNNModel()
+model = ResNet18()
 optimizer = Adam(model.parameters())
 learner = SupervisedLearner(
     train_loader, val_loader, model=model,
