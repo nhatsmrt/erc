935e88b62c91e39378630a2eb0aa56aaa7c77488
nhatsmrt <nhatsmrt@uw.edu>
2019-11-25 22:37:19 -0800

    Change model path and submission file.

--- test.py
+++ test.py
@@ -15,9 +15,9 @@ transform = Compose(
 )
 test_dataset = ERCDataRaw("data/", False)
 model = CNNModel()
-model.load_state_dict(torch.load('weights/model_CNN_small.pt', map_location=lambda storage, location: storage))
+model.load_state_dict(torch.load('weights/model.pt', map_location=lambda storage, location: storage))
 model.eval()
 
 
 emo = EmoRec(model, tta_transform=transform)
-emo.export_predictions(test_dataset, "data/submission_CNN_3.csv")
+emo.export_predictions(test_dataset, "data/submission_2.csv")
