12d00aa3b576ba0659413ef5458c58376198503c
nhatsmrt <nhatsmrt@uw.edu>
2019-11-26 16:57:03 -0800

    Update test script.

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,7 +1,5 @@
 import torch
 from torch import Tensor, nn
-from nntoolbox.vision.models import ImageClassifier
-from nntoolbox.utils import get_device
 from torch.utils.data import Dataset
 import numpy as np
 from numpy import ndarray
@@ -15,27 +13,35 @@ class DummyTransform:
     def __call__(self, input): return input
 
 
-class EmoRec(ImageClassifier):
-    def __init__(self, model: nn.Module, shared_transform=DummyTransform(), tta_transform=None, device=get_device()):
-        super().__init__(model, tta_transform, 0.0, device)
-        self.shared_transform = shared_transform
+class EmoRec:
+    """
+    Abstraction for an image classifier. Support user defined test time augmentation
+    """
+    def __init__(self, model, transform_main=DummyTransform(), tta_transform=None, tta_beta: float=0.4):
+        self._model = model
+        self._model.eval()
+        self._tta_transform = tta_transform
+        self._softmax = nn.Softmax(dim=1)
+        self.transform_main = transform_main
+        self.tta_beta = tta_beta
 
     @torch.no_grad()
-    def predict(self, image: Tensor, return_probs: bool=False, tries: int=5) -> ndarray:
+    def predict(self, image: Tensor, tries: int=5) -> ndarray:
         """
         Predict the classes or class probabilities of a batch of images
 
         :param image: image to be predicted
-        :param return_probs: INVALID FOR THIS CLASS.
         :param tries: number of tries for augmentation
         :return:
         """
         if self._tta_transform is not None:
-            version = [self._tta_transform(image) for _ in range(tries)]
+            version = [self.transform_main(image)] + [self._tta_transform(image) for _ in range(tries)]
             outputs = self._softmax(self._model(torch.stack(version, dim=0)))
-            outputs = outputs.mean(dim=0).argmax(0, keepdim=True)
+            weights = [self.tta_beta] + [(1 - self.tta_beta) / tries for _ in range(tries)]
+            weights = torch.from_numpy(np.array(weights)).to(outputs.dtype).to(outputs.device)[:, None]
+            outputs = (outputs * weights).sum(dim=0).argmax(0, keepdim=True)
         else:
-            outputs = self._model(image.unsqueeze(0)).argmax(1)
+            outputs = self._model(self.transform_main(image).unsqueeze(0)).argmax(1)
 
         return outputs.detach().cpu().numpy()
 
@@ -43,7 +49,6 @@ class EmoRec(ImageClassifier):
     def export_predictions(self, test_data: Dataset, path: str, tries: int=5):
         outputs = []
         for image in test_data:
-            image = self.shared_transform(image.to(self._device))
             outputs.append(self.predict(image, tries=tries))
 
         outputs = np.concatenate(outputs, axis=0)
--- test.py
+++ test.py
@@ -6,18 +6,27 @@ from torchvision.transforms import Compose, RandomCrop, ToPILImage, ToTensor
 from src.inference import EmoRec
 
 frequency = 16000
-transform = Compose(
+transform_tta = Compose(
     [
         RandomCropCenter(30000),
         MFCC(sample_rate=frequency),
         TimePad(280)
     ]
 )
+transform_main = Compose(
+    [
+        MFCC(sample_rate=frequency),
+        TimePad(280)
+    ]
+)
 test_dataset = ERCDataRaw("data/", False)
 model = CNNModel()
-model.load_state_dict(torch.load('weights/model.pt', map_location=lambda storage, location: storage))
+model.load_state_dict(torch.load('weights/model (18).pt', map_location=lambda storage, location: storage))
 model.eval()
 
 
-emo = EmoRec(model, tta_transform=transform)
-emo.export_predictions(test_dataset, "data/submission_2_tta.csv")
+# emo = EmoRec(model, transform_main=transform_main, tta_transform=transform_tta, tta_beta=1.0)
+# emo.export_predictions(test_dataset, "data/submission_2.csv")
+
+emo = EmoRec(model, transform_main=transform_main, tta_transform=transform_tta, tta_beta=0.6)
+emo.export_predictions(test_dataset, "data/submission_2_tta.csv", tries=10)
